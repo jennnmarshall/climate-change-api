886c92a63e6ff07e69b2d9b03cfaf6ad32c9e5f4
Jenn Marshall <jennyfever@gmail.com>
2023-02-17 13:20:48 -0700

    added code to index for web scrape

--- index.js
+++ index.js
@@ -1,6 +1,87 @@
 const PORT = 8000;
-const express = require('express');
-const axios = require('axios');
-const cheerio = require('cheerio');
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const newspapers = [
+  {
+    name: "cnn",
+    address: "https://www.cnn.com/specials/world/cnn-climate",
+    base: "http://www.cnn.com"
+  },
+  {
+    name: "nyt",
+    address: "https://www.nytimes.com/section/climate",
+    base: ''
+  },
+  {
+    name: "atlantic",
+    address: "https://www.theatlantic.com/projects/planet/",
+    base: ''
+  },
+  {
+    name: "guardian",
+    address: "https://www.theguardian.com/environment/climate-crisis",
+    base: ''
+  },
+];
 
 const app = express();
+
+const articles = [];
+
+newspapers.forEach(newspaper => {
+    axios.get(newspaper.address)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+
+            $('a:contains("climate")', html).each(function () {
+                const title = $(this).text()
+                const url = $(this).attr('href')
+
+                articles.push({
+                    title,
+                    url: newspaper.base + url,
+                    source: newspaper.name
+                })
+            })
+        })
+})
+
+app.get("/", (req, res) => {
+  res.json("Welcome to my Climate Change News API");
+});
+
+app.get("/news", (req, res) => {
+    res.json(articles)
+});
+
+app.get('/news/:newspaperId', async (req, res) => {
+    const newspaperId = req.params.newspaperId
+
+    const newspaperAddr = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].address
+    const newspaperBase = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].base
+
+    console.log(newspaperAddr)
+
+    axios.get(newspaperAddr)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+            const specificArticles = []
+
+            $('a:contains("climate")', html).each(function () {
+                const title = $(this).text()
+                const url = $(this).attr('href')
+                specificArticles.push({
+                    title,
+                    url: newspaperBase + url,
+                    source: newspaperId
+                })
+            })
+            res.json(specificArticles)
+        }).catch(err => console.log(err))
+})
+
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
